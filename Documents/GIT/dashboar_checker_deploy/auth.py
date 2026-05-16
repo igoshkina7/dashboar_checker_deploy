70187dff1abddbbdfde8564eed1eb1108a9595f7
igoshkina7 <71019642+igoshkina7@users.noreply.github.com>
2026-05-16 15:06:30 +0300

    Update auth.py

--- Documents/GIT/dashboar_checker_deploy/auth.py
+++ Documents/GIT/dashboar_checker_deploy/auth.py
@@ -2,11 +2,8 @@ import streamlit as st
 from playwright.sync_api import sync_playwright
 from settings import AUTH_FILE, HEADLESS
 
-
-
 def login_and_save_auth():
     try:
-        # берём секреты напрямую из Streamlit
         BASE_URL = st.secrets["BASE_URL"]
         USERNAME = st.secrets["USERNAME"]
         PASSWORD = st.secrets["PASSWORD"]
@@ -20,31 +17,22 @@ def login_and_save_auth():
             page = context.new_page()
 
             page.goto(f"{BASE_URL}/auth")
-
-            st.write(f"{BASE_URL}/auth")
+            st.write(f"Перешли на {BASE_URL}/auth")
 
             page.fill("input[type='text']", USERNAME)
             page.fill("input[type='password']", PASSWORD)
-
             st.write("LOGIN AND PASSWORD DONE")
             
             page.click("button:has-text('Войти')")
-
-            # ждём выхода из логина
             page.wait_for_url(lambda url: "/auth" not in url, timeout=30000)
 
-            #print("CURRENT_URL", page.url)
-            st.write("f"CURRENT_URL = {BASE_URL}"")
-
-            context.storage_state(path=page.url)
-
+            st.write(f"CURRENT_URL = {page.url}")   # исправлено: f-строка и page.url
+            context.storage_state(path=AUTH_FILE)   # исправлено: должен быть AUTH_FILE, а не page.url
             st.write("✅ AUTH SAVED")
-            #print("✅ AUTH SAVED")
 
             browser.close()
             return True
 
     except Exception as e:
-        st.write("f"LOGIN ERROR: {e}")
-        #print(f"LOGIN ERROR: {e}")
+        st.write(f"LOGIN ERROR: {e}")   # исправлено
         return False
