b8e920b0433fd3975effe13599eaaf4e74ca4b06
igoshkina7 <71019642+igoshkina7@users.noreply.github.com>
2026-05-16 14:44:38 +0300

    Update app.py

--- Documents/GIT/dashboar_checker_deploy/app.py
+++ Documents/GIT/dashboar_checker_deploy/app.py
@@ -9,42 +9,59 @@ st.title("📊 Dashboard Checker")
 
 if "log" not in st.session_state:
     st.session_state["log"]=""
-
-    try:
-            st.session_state["log"] += "⏳ RUN CHECKS\n"
     
-            from checker import run_checks
-            results = run_checks()
+def add_log(message: str):
+    st.session_state["log"] += message +"\n"
+    st.return
     
-            st.session_state["log"] += "✅ DONE\n"
-
-    except Exception as e:
-        st.session_state["log"] += f"❌ ERROR: {e}\n"
 
 def ensure_auth():
 
     # всегда создаём новую сессию
     if os.path.exists(AUTH_FILE):
-        os.remove(AUTH_FILE)
+        add_log("✅AUTH FILE EXISTS")
+        #os.remove(AUTH_FILE)
+        return True
 
     ok = login_and_save_auth()
 
     if not ok:
+        add_log("❌LOGIN FAILED")
         raise Exception("LOGIN FAILED")
 
+    add_log("✅LOGIN SUCCESS")
     return True
 
 if st.button("🔍 Запустить проверку"):
     try:
+        add_log("START CHECK")
         ensure_auth()
         st.info("⏳ Проверка дашбордов...")
+        add_log("⏳RUN CHECK")
         results = run_checks()
+        add_log("RESULTS CHECK")
         text = ""
         for r in results:
             text += f"{r.name}\n"
-            text += "✅ OK\n" if r.success else f"❌ {r.message}\n"
+            text += "✅ OK\n" 
+            if r.success:
+                add_log(f"✅{r.name} - OK")
+            else: 
+                add_log(f"❌{r.name} - {r.message}")
+                f"❌ {r.message}\n"
             text += "\n"
+
+        add_log("DONE")
         st.success("Готово")
         st.text(text)
+        
     except Exception as e:
+        add_log(f"ERROR: {e}")
         st.error(f"❌ Ошибка: {e}")
+
+st.subheader ("Лог выполнения")
+st.text_area(
+    "debug log"
+    value = st.session_state["log"]
+    height=300
+    )
